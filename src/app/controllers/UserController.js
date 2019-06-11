e6d308944962560a62327b5fff8adfe9ed50aa42
Lucas Ferreira <lucasfsouza182@gmail.com>
2019-06-10 23:08:08 -0300

    File Upload

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -6,8 +6,19 @@ class UserController {
   }
 
   async store (req, res) {
-    console.log(req.body)
-    await User.create(req.body)
+    const { filename: avatar } = req.file
+    console.log('body', req.body)
+    console.log('avatar', avatar)
+    const userBanco = { ...req.body, avatar }
+    console.log('user', userBanco)
+
+    await User.create({ ...req.body, avatar })
+      .then(function (user) {
+        console.log('success')
+      })
+      .catch(function (err) {
+        console.log(err, req.body)
+      })
 
     return res.redirect('/')
   }
